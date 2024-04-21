dec8c8ff19ae6bc5730c9d985fac54c2af800709
marievanderpool <marievanderpool2@gmail.com>
2024-04-21 17:37:52 -0500

    first commit

---
+++ mini_project.py
@@ -0,0 +1,51 @@
+# Introduction
+
+# In this project, you will apply your Python programming skills to create a functional To-Do List 
+# Application from scratch. The objective of this project is to reinforce your understanding of Python
+#  syntax, data types, control structures, functions, and error handling while building a practical 
+# and interactive application.
+
+# Project Requirements
+# User Interface (UI):
+# Create a command-line interface (CLI) for the To-Do List Application.
+# Display a welcoming message and a menu with the following options:
+# Welcome to the To-Do List App!
+#   Menu:
+    # 1. Add a task
+    # 2. View tasks
+    # 3. Mark a task as complete
+    # 4. Delete a task
+    # 5. Quit
+
+# To-Do List Features:
+# Implement the following features for the To-Do List:
+# Adding a task (by default “Incomplete”).
+# View the list of tasks
+# Viewing the list of tasks with their titles and statuses (e.g., "Incomplete" or "Complete"). (BONUS)
+# Deleting a task.
+# Quitting the application.
+# Marking a task as complete. 
+
+# 1.) User Interaction:
+# Allow users to interact with the application by selecting menu options using input().
+# Implement input validation to handle unexpected user input gracefully.
+
+# 2.) Error Handling:
+# Implement error handling using try, except, else, and finally blocks to handle potential issues.
+
+# 3.) Code Organization:
+# Organize your code into functions to promote modularity and readability.
+# Use meaningful function names with appropriate comments and docstrings for clarity.
+
+# 4.) Testing and Debugging:
+# Thoroughly test your application to identify and fix any bugs.
+# Consider edge cases, such as empty task lists or incorrect user input.
+
+# 5.) Optional Features (Bonus):
+# If you feel adventurous, you can add extra features like task priorities, due dates, 
+# or color-coding tasks based on their status.
+
+# 6.) GitHub Repository:
+# Create a GitHub repository for your project.
+# Commit your code to the repository regularly.
+# Include a link to your GitHub repository in your project documentation
